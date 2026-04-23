53726da1861559f9b0028b28777a1a44bd24b2b8
Victor Hayashi <vtakashi@hotmail.com>
2026-04-23 06:49:01 -0300

    feat: dois leds no blink.ino

--- blink.ino
+++ blink.ino
@@ -26,12 +26,15 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(8, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(8, LOW);
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(8, HIGH);
   delay(1000);                       // wait for a second
 }
